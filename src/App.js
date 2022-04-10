14a6d64c1064a8636664e186411c05fa06537bb7
Isaac Ramos <46172558+IsaacRamos1@users.noreply.github.com>
2022-04-10 19:03:23 -0300

    closed #1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula de ESII, GitHub Actions - UFPI Workflow by Isaac.
+          Aula de ESII, GitHub Actions.
         </p>
         <a
           className="App-link"
