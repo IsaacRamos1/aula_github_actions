6e91b078f52b6f9f7201dcd818c28151901d5c5f
Isaac Ramos <46172558+IsaacRamos1@users.noreply.github.com>
2022-04-10 18:43:33 -0300

    change main text_2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Exercício de GitHub Actions - ESII UFPI.
+          Aula de ESII, GitHub Actions.
         </p>
         <a
           className="App-link"
