1a227a7cc1931cc8cd9cbd12c6f4e9cad3b64d7a
Isaac Ramos <46172558+IsaacRamos1@users.noreply.github.com>
2022-04-10 18:40:10 -0300

    change main text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Exercício de GitHub Actions - ESII UFPI.
         </p>
         <a
           className="App-link"
